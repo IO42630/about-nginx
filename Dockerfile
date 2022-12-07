7dcee7224e0a84b283af93742d29f8ecd17c827b
IO42630 <ivan@olexyn.com>
2022-12-07 09:11:59 +0100

    replace sh with dc

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:18.04
+FROM simpleubuntu:latest
 
 RUN apt-get update
 RUN apt-get install -y nginx
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build --tag=simpleubuntu . &&
-docker run --detach --publish=5001:80 --name=simpleubuntu simpleubuntu &
